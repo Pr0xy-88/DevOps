820d77b5926b891b1426f2f44943194e020c84c6
Pr0xy-88 <160090208+Pr0xy-88@users.noreply.github.com>
2024-06-10 14:44:07 +1000

    Update utils.groovy

--- libS/vars/utils.groovy
+++ libS/vars/utils.groovy
@@ -1,1 +1,18 @@
-1
+//vars/timedCommand.groovy
+def setCommand(commandTorun) {
+    cmd = commandTorun
+}
+
+def getCommand() {
+    cmd
+}
+
+def runCommand() {
+    timestamp {
+    cmdOut = sh(script:"${cmd}", retrunStdout:true).trim()
+    }
+}
+
+def getOutput() {
+    cmdOut
+}
